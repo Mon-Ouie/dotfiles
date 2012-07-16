7049a7278d1c87197821b40bbcde8c5b576471bc
Mon ouïe <mon.ouie@gmail.com>
2012-07-16 17:35:25 +0200

    Moved weechat script to ~/code/dotfiles

---
+++ weechat.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+create_irc_session () {
+    tmux new-session -d -s irc
+    tmux new-window -t irc:2 -n "irc" "TERM=screen-256color weechat-curses"
+    tmux attach -t irc
+}
+
+if [ `tmux has-session -t irc` ]; then
+    tmux attach -t irc
+else
+    case `hostname` in
+        *-laptop) # maybe use SSH
+            read -p "Host of the computer running weechat? (localhost) " remote
+            echo $remote
+
+            if [ "$remote" = "" ]; then
+                create_irc_session
+            else
+                ssh $remote -t "LC_ALL=en_EN.UTF-8 tmux attach -t irc"
+            fi
+            ;;
+
+        *-desktop) create_irc_session;;
+    esac
+fi
