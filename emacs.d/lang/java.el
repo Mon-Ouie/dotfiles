5a620e15a867279067be5f1896f8145150f78804
Mon ouïe <mon.ouie@gmail.com>
2012-08-29 00:09:46 +0200

    Disabled flymake for Java

--- emacs.d/lang/java.el
+++ emacs.d/lang/java.el
@@ -7,6 +7,8 @@
 (require 'ac-emacs-eclim-source)
 (add-hook 'eclim-mode-hook
           (lambda ()
+            (flymake-mode -1)
+
             (add-to-list 'ac-sources 'ac-source-emacs-eclim)
             (add-to-list 'ac-sources 'ac-source-emacs-eclim-c-dot)))
 
