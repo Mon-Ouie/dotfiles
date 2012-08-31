e00fbdcd0521d649093d693c63bec6851ebba9f8
Mon ouïe <mon.ouie@gmail.com>
2012-08-31 19:15:17 +0200

    Made gnus prettier

--- emacs.d/mail.el
+++ emacs.d/mail.el
@@ -20,6 +20,31 @@
 (setq gnus-thread-sort-functions
       '(gnus-thread-sort-by-most-recent-date))
 
+(setq gnus-summary-to-prefix "→"
+      gnus-summary-newsgroup-prefix "⇶"
+      ;; Marks
+      gnus-ticked-mark ?⚑
+      gnus-dormant-mark ?⚐
+      gnus-expirable-mark ?♻
+      gnus-read-mark ?✓
+      gnus-del-mark ?✗
+      gnus-killed-mark ?☠
+      gnus-replied-mark ?↺
+      gnus-forwarded-mark ?↪
+      gnus-cached-mark ?☍
+      gnus-recent-mark ?✩
+      gnus-unseen-mark ?★
+      gnus-unread-mark ?✉
+      gnus-score-over-mark ?↑           ; ↑ ☀
+      gnus-score-below-mark ?↓         ; ↓ ☂
+      gnus-sum-thread-tree-false-root " ◌ "
+      gnus-sum-thread-tree-single-indent "◎ "
+      gnus-sum-thread-tree-indent "   "
+      gnus-sum-thread-tree-root "● "
+      gnus-sum-thread-tree-leaf-with-other "├─▶ "
+      gnus-sum-thread-tree-single-leaf     "└─▶ " ; "╰─►"
+      gnus-sum-thread-tree-vertical        "│ ")
+
 (setq user-mail-address "mon.ouie@gmail.com")
 (setq user-full-name    "Mon ouïe")
 
