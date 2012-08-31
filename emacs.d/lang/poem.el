082850cb255ca1fc5c7fc3fef6d1cab68a348dc7
Mon ouïe <mon.ouie@gmail.com>
2012-08-31 10:00:07 +0200

    Fixed deft-text-mode

--- emacs.d/lang/poem.el
+++ emacs.d/lang/poem.el
@@ -1,9 +1,11 @@
 (require 'deft)
 (require 'poem-mode)
 
+(add-hook 'poem-mode-hook 'turn-off-auto-fill)
+
 (defun poem ()
   (interactive)
   (setq deft-extension "poem")
   (setq deft-directory "~/doc/poems/")
-  (setq deft-text-mode 'text-mode)
+  (setq deft-text-mode 'poem-mode)
   (deft))
