29c8e45d8ad7b5262d337483de831707d5bd95a8
Mon ouïe <mon.ouie@gmail.com>
2013-06-22 00:45:36 +0200

    Removed transparency and adjust color settings

--- emacs.d/look.el
+++ emacs.d/look.el
@@ -3,9 +3,11 @@
 (menu-bar-mode -1)
 (if (boundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
-;; Enable transparency.
-(set-frame-parameter (selected-frame) 'alpha '(70 70))
-(add-to-list 'default-frame-alist '(alpha 70 70))
+(let ((opacity 100))
+  (set-frame-parameter (selected-frame) 'alpha
+                       (list opacity opacity))
+  (add-to-list 'default-frame-alist
+               (list 'alpha opacity opacity)))
 
 (show-paren-mode t)
 (column-number-mode t)
@@ -29,19 +31,24 @@
 (set-face-foreground 'magit-diff-add "green")
 (set-face-attribute 'highlight
                     nil
-                    :background "#333333" :underline t)
+                    :background "#333333" :underline nil)
+(set-face-background 'magit-diff-hunk-header "#333333")
+(set-face-foreground 'magit-diff-hunk-header "#afafaf")
+
+(set-face-background 'magit-diff-file-header "#222222")
+(set-face-foreground 'magit-diff-file-header "#bfbfbf")
 
 (require 'linum)
-(set-face-foreground 'linum "#FFFFFF")
+(set-face-foreground 'linum "#7f7f7f")
 
 ;; Colors in compile-mode
 (require 'ansi-color)
 
 (ansi-color-map-update 'ansi-color-names-vector
                        ["#3b3b3b" "#ff6b6b" "#a3d46e" "#eaca75" "#435e87"
-                        "#cf8243" "#789ec6" "#ffffff"])
+                        "#cf8243" "#789ec6" "#7f7f7f"])
 
-(defun colorize-compilation-buffer()
+(defun colorize-compilation-buffer ()
   (toggle-read-only)
   (ansi-color-apply-on-region (point-min) (point-max))
   (toggle-read-only))
