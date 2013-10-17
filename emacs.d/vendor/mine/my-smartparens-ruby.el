bb7e3ccca91b3af80cead5d814cd405f7c06f709
Mon ouïe <mon.ouie@gmail.com>
2013-10-17 02:42:10 +0200

    Added a rule for until … end in smartparens-oz

--- emacs.d/vendor/mine/my-smartparens-ruby.el
+++ emacs.d/vendor/mine/my-smartparens-ruby.el
@@ -2,18 +2,21 @@
 
 (require 'smartparens-ruby)
 
-(defun sp-ruby-post-case-insert (id action context)
+(defun sp-ruby-post-insert-newline (id action context)
   (when (string= action 'insert)
-    (cond
-     ((equal id "case")
-      (save-excursion (newline-and-indent))
-      (insert " ")))))
+    (save-excursion (newline-and-indent))
+    (insert " ")))
 
 (sp-with-modes '(ruby-mode)
   (sp-local-pair "case" "end"
                  :when '(("SPC" "RET" "<evil-ret>"))
                  :unless '(sp-ruby-in-string-or-word-p)
                  :pre-handlers '(sp-ruby-pre-handler)
-                 :post-handlers '(sp-ruby-post-case-insert)))
+                 :post-handlers '(sp-ruby-post-insert-newline))
+  (sp-local-pair "until" "end"
+                 :when '(("SPC" "RET" "<evil-ret>"))
+                 :unless '(sp-ruby-in-string-or-word-p)
+                 :pre-handlers '(sp-ruby-pre-handler)
+                 :post-handlers '(sp-ruby-post-insert-newline)))
 
 (provide 'my-smartparens-ruby)
