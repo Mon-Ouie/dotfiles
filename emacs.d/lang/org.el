ea054159e8407c2e6be27763e9b0efc90da2ddfc
Mon ouïe <mon.ouie@gmail.com>
2014-03-28 22:34:59 +0100

    Changed size of formulae in org-mode

--- emacs.d/lang/org.el
+++ emacs.d/lang/org.el
@@ -51,7 +51,7 @@
 (setq org-format-latex-options
       '(:foreground default
         :background default
-        :scale 1.3
+        :scale 2.0
         :html-foreground "Black"
         :html-background "Transparent"
         :html-scale 1.0
