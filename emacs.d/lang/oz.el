5c18892b4111790b1f492ea4078cb3b68e20d6ac
Mon ouïe <mon.ouie@gmail.com>
2014-11-18 18:18:51 +0100

    Allowed Emacs to load when Oz is not setup

--- emacs.d/lang/oz.el
+++ emacs.d/lang/oz.el
@@ -1,37 +1,38 @@
 (unless (getenv "OZHOME") (setenv "OZHOME" "/usr/local"))
 
-(setq auto-mode-alist
-      (append '(("\\.oz\\'" . oz-mode)
-                ("\\.ozg\\'" . oz-gump-mode))
-              auto-mode-alist))
+(when (executable-find "ozc")
+  (setq auto-mode-alist
+        (append '(("\\.oz\\'" . oz-mode)
+                  ("\\.ozg\\'" . oz-gump-mode))
+                auto-mode-alist))
 
-(require 'oz)
+  (require 'oz)
 
-(setq oz-change-title nil)
+  (setq oz-change-title nil)
 
-(define-key oz-mode-map (kbd "M-p") nil)
-(define-key oz-mode-map (kbd "M-n") nil)
+  (define-key oz-mode-map (kbd "M-p") nil)
+  (define-key oz-mode-map (kbd "M-n") nil)
 
-(define-key oz-mode-map (kbd "C-c C-l") 'oz-feed-line)
-(define-key oz-mode-map (kbd "C-C C-b") 'oz-feed-buffer)
-(define-key oz-mode-map (kbd "C-c C-r") 'oz-feed-region)
+  (define-key oz-mode-map (kbd "C-c C-l") 'oz-feed-line)
+  (define-key oz-mode-map (kbd "C-C C-b") 'oz-feed-buffer)
+  (define-key oz-mode-map (kbd "C-c C-r") 'oz-feed-region)
 
-;; Alignment rules
+  ;; Alignment rules
 
-(require 'align)
+  (require 'align)
 
-(add-to-list 'align-rules-list
-             '(oz-then
-               (regexp  . "\\(\\s-+\\)then")
-               (modes   . '(oz-mode))))
+  (add-to-list 'align-rules-list
+               '(oz-then
+                 (regexp  . "\\(\\s-+\\)then")
+                 (modes   . '(oz-mode))))
 
-(add-to-list 'align-rules-list
-             '(oz-if-elseif-else
-               (regexp . "\\b\\(?:if\\|elseif\\)\\b\\(\\s-+\\)[^\\s]")
-               (modes  . '(oz-mode))))
+  (add-to-list 'align-rules-list
+               '(oz-if-elseif-else
+                 (regexp . "\\b\\(?:if\\|elseif\\)\\b\\(\\s-+\\)[^\\s]")
+                 (modes  . '(oz-mode))))
 
-(add-to-list 'align-dq-string-modes    'oz-mode)
-(add-to-list 'align-sq-string-modes    'oz-mode)
-(add-to-list 'align-open-comment-modes 'oz-mode)
+  (add-to-list 'align-dq-string-modes    'oz-mode)
+  (add-to-list 'align-sq-string-modes    'oz-mode)
+  (add-to-list 'align-open-comment-modes 'oz-mode)
 
-(add-hook 'oz-mode-hook 'turn-on-auto-fill)
+  (add-hook 'oz-mode-hook 'turn-on-auto-fill))
