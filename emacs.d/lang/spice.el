7a49af9e1b2ae6227f80c0d4360a96767f8ff464
Mon ouïe <mon.ouie@gmail.com>
2014-11-27 21:15:48 +0100

    spice.el: Added utility functions to process data

--- emacs.d/lang/spice.el
+++ emacs.d/lang/spice.el
@@ -8,3 +8,32 @@
 (define-key calc-mode-map (kbd "G a") 'spice-graph-ac)
 
 (setq calc-gnuplot-default-device "qt")
+
+(defun spice<= (&rest xs)
+  (every (lambda (x y)
+           (let ((a (math-simplify (math-remove-units x)))
+                 (b (math-simplify (math-remove-units y))))
+             (not (math-lessp b a)))) xs (cdr xs)))
+
+(defun spice>= (&rest xs)
+  (every (lambda (x y)
+           (let ((a (math-simplify (math-remove-units x)))
+                 (b (math-simplify (math-remove-units y))))
+             (or (math-lessp b a) (math-equal a b)))) xs (cdr xs)))
+
+(defun calcFunc-vfind (xs ys sought-y &optional edges)
+  (unless edges (setq edges '(rising falling)))
+  (cons 'vec
+        (loop for (x0 x1 . xr) on (cdr xs)
+              for (y0 y1 . yr) on (cdr ys)
+              while (and x0 x1 y0 y1)
+              when (or (and (spice<= y0 sought-y y1) (member 'rising  edges))
+                       (and (spice>= y0 sought-y y1) (member 'falling edges)))
+              collect (let* ((f (if (math-equal y0 y1) 0
+                                  (math-div (math-sub sought-y y0)
+                                            (math-sub y1 y0))))
+                             (x (math-add x0 (math-mul f (math-sub x1 x0)))))
+                        (math-simplify x)))))
+
+(defun calcFunc-vreverse (ys)
+  (cons 'vec (reverse (cdr ys))))
